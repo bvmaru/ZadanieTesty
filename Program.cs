0a62ed98f31c3c8336d17be2125ec80b0a74731b
Marcel <bvmaru@proton.me>
2023-02-19 14:34:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieTesty
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+              LiczbyPierwsze test = new LiczbyPierwsze();
+              Console.WriteLine(test.Sprawdzenie("3"));
+              Console.ReadLine();
+
+        }
+    }
+}
